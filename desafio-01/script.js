12404baaba45a444079dbfc46671e9961197f332
Igor Gazineo <gaumgazineo@hotmail.com>
2024-04-05 04:32:22 -0300

    putting css and js into the html file

--- desafio-01/script.js
+++ desafio-01/script.js
@@ -1,7 +1,1 @@
-const secondaryTitles = document.querySelectorAll("h2");
 
-secondaryTitles.forEach((title) => {
-  title.addEventListener("click", ({ target }) => {
-    target.style.color = "red";
-  });
-});
